624e42f70612a70cb598b44142d54150467053e3
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-16 16:17:33 +0000

    16 Apr 2023, 21:47

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,41 @@ import problemsArray from '../data'
 import Problem from './Problem'
 
 const Home = () => {
+
+    const styleClass={
+      tableData_1:{
+        minWidth:"10vw",
+        padding:"2rem",
+        backgroundColor:"grey"
+      },
+       tableData_2:{
+        minWidth:"10vw",
+        padding:"2rem",
+        backgroundColor:"green"
+      },
+       tableData_3:{
+        minWidth:"10vw",
+        padding:"2rem",
+        backgroundColor:"skyBlue"
+      }
+    }
   return (<>
 
     <div>Home</div>
     <>
+
     <table>
+      <tr>
+        <td style={styleClass.tableData_1}>
+            Title
+        </td>
+        <td style={styleClass.tableData_2}>
+            Acceptance %
+        </td>
+        <td style={styleClass.tableData_3}>
+          Difficulty
+        </td>
+     </tr>
      {problemsArray.map(problem=><Problem key={problem.title} {...problem} />)}
     </table>
     </>
