08026de6834e8fafc7ea97bbfa527eb5f4fe29fa
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-16 16:15:32 +0000

    16 Apr 2023, 21:45

--- src/components/Problem.jsx
+++ src/components/Problem.jsx
@@ -7,26 +7,30 @@ export default function ProblemStatement(props) {
     const styleClass={
       tableData_1:{
         minWidth:"10vw",
-        padding:"2rem"
+        padding:"2rem",
+        backgroundColor:"grey"
       },
        tableData_2:{
         minWidth:"10vw",
-        padding:"2rem"
+        padding:"2rem",
+        backgroundColor:"green"
       },
        tableData_3:{
         minWidth:"10vw",
-        padding:"2rem"
+        padding:"2rem",
+        backgroundColor:"skyBlue"
       }
     }
+
     return <tr>
-        <td style={styleClass.tableData}>
+        <td style={styleClass.tableData_1}>
             {title}
         </td>
-        <td style={styleClass.tableData}>
+        <td style={styleClass.tableData_2}>
             {acceptance}
         </td>
-        <td style={styleClass.tableData}>
+        <td style={styleClass.tableData_3}>
             {difficulty}
         </td>
-    </tr>
+     </tr>
 }
