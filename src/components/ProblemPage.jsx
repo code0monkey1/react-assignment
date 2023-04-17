eea97356aa3d64235181b46c69eacf9d1834b651
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-17 05:10:35 +0000

    17 Apr 2023, 10:40

---
+++ src/components/ProblemPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const Problem = () => {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <h1>Problem {id}</h1>
+      {/* Your problem content goes here */}
+    </div>
+  );
+};
+
+export default Problem;
